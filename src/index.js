329f3308863d71e51797f1f78bf7f3d86119a2f6
anickel101 <anson.nickel@gmail.com>
2020-09-17 13:32:49 -0400

    1/3 Deliverables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  const toysUrl = "http://localhost:3000/toys";
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +16,35 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const getToys = () => {
+    fetch(toysUrl)
+    .then(response => response.json())
+    .then(toyCollection => renderToys(toyCollection))
+  }
+
+  const renderToys = (toyCollection) => {
+    for (const toy of toyCollection) {
+      renderToy(toy);
+    }
+  }
+
+  const renderToy = (toy) => {
+    let toyCard = document.createElement("div")
+    toyCard.classList.add("card")
+
+    let toyContainer = document.querySelector("#toy-collection")
+    toyCard.innerHTML =     
+    `<h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>`
+    
+    toyContainer.append(toyCard)
+  }
+
+  getToys();
+
 });
+
+
