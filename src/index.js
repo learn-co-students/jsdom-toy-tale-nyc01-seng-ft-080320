b01135abb1e6a0367162aded63bb63f59fed7503
kevinpark07 <kevin.park07@yahoo.com>
2020-09-17 20:35:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,72 @@
 let addToy = false;
+let toyMap = {};
+
+function Toy (name, image, id, likes) {
+  this.name = name;
+  this.image = image;
+  this.id = id
+  this.likes = likes;
+}
+
+function toyFactory(toyObj) {
+  let newToyDiv = document.createElement('div');
+  let toyCollection = document.querySelector('#toy-collection')
+    newToyDiv.setAttribute('class', 'card');
+
+  newToyDiv.innerHTML = `
+    <h2>${toyObj.name}</h2>
+    <img src="${toyObj.image}" class = 'toy-avatar' />
+    <p>${toyObj.likes}</p>
+    <button class="like-btn" id=${toyObj.id}>Like <3</button>
+    `
+    toyCollection.append(newToyDiv);
+    
+
+
+  
+}
+
+function renderToys(toyArray) {
+  for(let toy of toyArray) {
+    toyFactory(toy)
+    
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+
+  document.addEventListener('click', e => {
+    if (e.target.class = 'like-btn') {
+      const likePTag = e.target.previousElementSibling;
+      let currentLikes = parseInt(likePTag.textContent) + 1;
+      
+
+      fetch(`http://localhost:3000/toys/${e.target.id}`,{
+
+        method: "PATCH",
+
+        headers: {
+          "Content-Type": "application/json",
+          'Accept': "application/json"
+        },
+      
+        body: JSON.stringify({
+
+          "likes": currentLikes
+        })
+      })
+      .then(resp => resp.json)
+      .then((likes)=> {
+        
+        likePTag.textContent = currentLikes;
+
+      })
+    }
+  })
+  
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -12,141 +76,50 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+
+    
+
   });
-
-  // ---------------------------
-
-  const baseUrl = "http://localhost:3000/toys/"
   
-  const renderToys = toys => {
-    const toyCollection = document.querySelector('#toy-collection')
-    for(let toy of toys){
-      let toyDiv = renderToy(toy)
-      toyCollection.append(toyDiv)
-    }
-  }
+  function addNewToy() {
+    
+    const toyForm = document.querySelector('.add-toy-form');
 
-  const renderToy = toy => {
-    const toyDiv = document.createElement('div')
-    toyDiv.classList.add('card')
+    toyForm.addEventListener('submit', e => {
+      e.preventDefault();
 
-    toyDiv.innerHTML = `
-      <h2>${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" />
-      <p>${toy.likes} Likes </p>
-      <button class="like-btn" data-toy-id="${toy.id}">Like <3</button>
-    `
-
-    // toyCollection.append(toyDiv)
-    return toyDiv
-  }
-  
-  const getToys = () => {
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then(toys => renderToys(toys))
-  }
-  
-  const submitHandler = () => {
-    const form = document.querySelector(".add-toy-form")
-
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-
-      const name = form.name.value
-      const image = form.image.value
-
-      const toy = {
-        name: name,
-        image: image,
-        likes: 0,
-      }
-
-      form.reset()
-
-      // get it on the DOM
-      // const toyDiv = renderToy(toy)
-      // document.querySelector('#toy-collection').append(toyDiv)
       
-      // save it to the DB
-      const options = {
+      fetch('http://localhost:3000/toys', {
         method: "POST",
         headers: {
-          "content-type": "application/json",
-          "accept": "application/json"
+          "Content-Type": "application/json",
+          "Accept": "application/json"
         },
-        body: JSON.stringify(toy)
-      }
-      
-      fetch(baseUrl, options)
-      .then(response => response.json())
-      .then(toy => {
-        const toyDiv = renderToy(toy)
-        document.querySelector('#toy-collection').append(toyDiv)
+        body: JSON.stringify({
+          name: toyForm.name.value,
+          image: toyForm.image.value,
+          likes: 0
+        })
       })
-
-    })
+      .then(resp => resp.json())
+      .then(newToy => {
+        console.log(newToy)
+        toyFactory(newToy);
+      });
+      
+    });
   }
 
-const clickHandler = () => {
-  document.addEventListener('click', e => {
-    if(e.target.matches('.like-btn')){
-      const button = e.target
-      const toyId = button.dataset.toyId
-      const likesEl = button.previousElementSibling
-      const newLikes = parseInt(likesEl.textContent) + 1
-
-      // pessimistic rendering
-      const options = {
-        method: "PATCH",
-        headers: {
-          "content-type": "application/json",
-          "accept": "application/json"
-        },
-        body: JSON.stringify({ likes: newLikes })
-      }
-
-      fetch(baseUrl + toyId, options)
-      .then(response => response.json())
-      .then(toy => {
-        likesEl.textContent = `${toy.likes} Likes`
-      })
-
-    }
-  })
-}
   
-  getToys()
-  submitHandler()
-  clickHandler()
+
+
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toys => {
+      console.log(toys)
+      renderToys(toys);
+    });
+
+    addNewToy();
+
 });
-
-
-
-/*
-Fetch Andy's Toys
-
-On the index.html page, there is a div with the id "toy-collection."
-
-When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
-
-Add Toy Info to the Card
-Each card should have the following child elements:
-
-h2 tag with the toy's name
-img tag with the src of the toy's image attribute and the class name "toy-avatar"
-p tag with how many likes that toy has
-button tag with a class "like-btn"
-After all of that, the toy card should resemble:
-
-  <div class="card">
-    <h2>Woody</h2>
-    <img src=toy_image_url class="toy-avatar" />
-    <p>4 Likes </p>
-    <button class="like-btn">Like <3</button>
-  </div>
-
-
-  https://images-na.ssl-images-amazon.com/images/I/81BDQZEXrpL._AC_SL1500_.jpg
-
-*/
